f2e69c670455f053dcb97e5d3104fdd73dc64a6f
Alif Pahreza <agzmv123@gmail.com>
2025-02-21 20:27:04 +0800

    add: footer section

---
+++ components/footer.tsx
@@ -0,0 +1,128 @@
+import Link from 'next/link';
+import React from 'react';
+
+export default function Footer() {
+	return (
+		<div className='flex justify-between items-center border-t border-line py-4'>
+			<div key='icon social media' className='flex items-center gap-2'>
+				<Link
+					href='https://www.linkedin.com/in/alifpahreza/'
+					className='hover:-translate-y-1 transition-all transform grayscale hover:grayscale-0'
+					target='_blank'
+				>
+					<svg
+						xmlns='http://www.w3.org/2000/svg'
+						x='0px'
+						y='0px'
+						width='31'
+						height='31'
+						viewBox='0 0 48 48'
+					>
+						<path
+							fill='#0288D1'
+							d='M42,37c0,2.762-2.238,5-5,5H11c-2.761,0-5-2.238-5-5V11c0-2.762,2.239-5,5-5h26c2.762,0,5,2.238,5,5V37z'
+						></path>
+						<path
+							fill='#FFF'
+							d='M12 19H17V36H12zM14.485 17h-.028C12.965 17 12 15.888 12 14.499 12 13.08 12.995 12 14.514 12c1.521 0 2.458 1.08 2.486 2.499C17 15.887 16.035 17 14.485 17zM36 36h-5v-9.099c0-2.198-1.225-3.698-3.192-3.698-1.501 0-2.313 1.012-2.707 1.99C24.957 25.543 25 26.511 25 27v9h-5V19h5v2.616C25.721 20.5 26.85 19 29.738 19c3.578 0 6.261 2.25 6.261 7.274L36 36 36 36z'
+						></path>
+					</svg>
+				</Link>
+				<Link
+					href='https://www.instagram.com/eunlipp_?igsh=djIxMnAzbHdxcHdk'
+					className='hover:-translate-y-1 transition-all transform grayscale hover:grayscale-0'
+					target='_blank'
+				>
+					<svg
+						xmlns='http://www.w3.org/2000/svg'
+						x='0px'
+						y='0px'
+						width='31'
+						height='31'
+						viewBox='0 0 48 48'
+					>
+						<radialGradient
+							id='yOrnnhliCrdS2gy~4tD8ma_Xy10Jcu1L2Su_gr1'
+							cx='19.38'
+							cy='42.035'
+							r='44.899'
+							gradientUnits='userSpaceOnUse'
+						>
+							<stop offset='0' stopColor='#fd5'></stop>
+							<stop offset='.328' stopColor='#ff543f'></stop>
+							<stop offset='.348' stopColor='#fc5245'></stop>
+							<stop offset='.504' stopColor='#e64771'></stop>
+							<stop offset='.643' stopColor='#d53e91'></stop>
+							<stop offset='.761' stopColor='#cc39a4'></stop>
+							<stop offset='.841' stopColor='#c837ab'></stop>
+						</radialGradient>
+						<path
+							fill='url(#yOrnnhliCrdS2gy~4tD8ma_Xy10Jcu1L2Su_gr1)'
+							d='M34.017,41.99l-20,0.019c-4.4,0.004-8.003-3.592-8.008-7.992l-0.019-20	c-0.004-4.4,3.592-8.003,7.992-8.008l20-0.019c4.4-0.004,8.003,3.592,8.008,7.992l0.019,20	C42.014,38.383,38.417,41.986,34.017,41.99z'
+						></path>
+						<radialGradient
+							id='yOrnnhliCrdS2gy~4tD8mb_Xy10Jcu1L2Su_gr2'
+							cx='11.786'
+							cy='5.54'
+							r='29.813'
+							gradientTransform='matrix(1 0 0 .6663 0 1.849)'
+							gradientUnits='userSpaceOnUse'
+						>
+							<stop offset='0' stopColor='#4168c9'></stop>
+							<stop offset='.999' stopColor='#4168c9' stopOpacity='0'></stop>
+						</radialGradient>
+						<path
+							fill='url(#yOrnnhliCrdS2gy~4tD8mb_Xy10Jcu1L2Su_gr2)'
+							d='M34.017,41.99l-20,0.019c-4.4,0.004-8.003-3.592-8.008-7.992l-0.019-20	c-0.004-4.4,3.592-8.003,7.992-8.008l20-0.019c4.4-0.004,8.003,3.592,8.008,7.992l0.019,20	C42.014,38.383,38.417,41.986,34.017,41.99z'
+						></path>
+						<path
+							fill='#fff'
+							d='M24,31c-3.859,0-7-3.14-7-7s3.141-7,7-7s7,3.14,7,7S27.859,31,24,31z M24,19c-2.757,0-5,2.243-5,5	s2.243,5,5,5s5-2.243,5-5S26.757,19,24,19z'
+						></path>
+						<circle cx='31.5' cy='16.5' r='1.5' fill='#fff'></circle>
+						<path
+							fill='#fff'
+							d='M30,37H18c-3.859,0-7-3.14-7-7V18c0-3.86,3.141-7,7-7h12c3.859,0,7,3.14,7,7v12	C37,33.86,33.859,37,30,37z M18,13c-2.757,0-5,2.243-5,5v12c0,2.757,2.243,5,5,5h12c2.757,0,5-2.243,5-5V18c0-2.757-2.243-5-5-5H18z'
+						></path>
+					</svg>
+				</Link>
+				<Link
+					href='https://github.com/Eunlip'
+					className='hover:-translate-y-1 transition-all transform grayscale hover:grayscale-0'
+					target='_blank'
+				>
+					<svg
+						xmlns='http://www.w3.org/2000/svg'
+						x='0px'
+						y='0px'
+						width='31'
+						height='31'
+						viewBox='0,0,256,256'
+					>
+						<g
+							fill='#ffffff'
+							fillRule='nonzero'
+							stroke='none'
+							strokeWidth='1'
+							strokeLinecap='butt'
+							strokeLinejoin='miter'
+							strokeMiterlimit='10'
+							strokeDasharray=''
+							strokeDashoffset='0'
+							fontFamily='none'
+							fontWeight='none'
+							fontSize='none'
+							textAnchor='none'
+							style={{ mixBlendMode: 'normal' }}
+						>
+							<g transform='scale(4,4)'>
+								<path d='M32,10c-12.15,0 -22,9.85 -22,22c0,12.15 9.85,22 22,22c12.15,0 22,-9.85 22,-22c0,-12.15 -9.85,-22 -22,-22zM32,14c9.941,0 18,8.059 18,18c0,8.23871 -5.54128,15.16934 -13.0957,17.30664c-0.0928,-0.19124 -0.15645,-0.40072 -0.15039,-0.63867c0.031,-1.209 0,-4.03041 0,-5.06641c0,-1.778 -1.125,-3.03906 -1.125,-3.03906c0,0 8.82422,0.09959 8.82422,-9.31641c0,-3.633 -1.89844,-5.52539 -1.89844,-5.52539c0,0 0.9973,-3.87844 -0.3457,-5.52344c-1.505,-0.163 -4.20056,1.43755 -5.35156,2.18555c0,0 -1.82342,-0.74805 -4.85742,-0.74805c-3.034,0 -4.85742,0.74805 -4.85742,0.74805c-1.151,-0.748 -3.84656,-2.34755 -5.35156,-2.18555c-1.342,1.645 -0.3457,5.52344 -0.3457,5.52344c0,0 -1.89844,1.89044 -1.89844,5.52344c0,9.416 8.82422,9.31836 8.82422,9.31836c0,0 -1.00476,1.14381 -1.10547,2.7832c-0.58969,0.20793 -1.39349,0.45313 -2.16016,0.45313c-1.85,0 -3.25548,-1.79691 -3.77148,-2.62891c-0.508,-0.821 -1.54948,-1.50977 -2.52148,-1.50977c-0.64,0 -0.95312,0.3215 -0.95312,0.6875c0,0.366 0.89823,0.62083 1.49023,1.29883c1.248,1.43 1.22488,4.64648 5.67188,4.64648c0.5258,0 1.47056,-0.1211 2.22461,-0.22461c-0.00417,1.00955 -0.0159,1.97778 0,2.59766c0.00586,0.23869 -0.05897,0.44894 -0.15234,0.64063c-7.55349,-2.1379 -13.09375,-9.0686 -13.09375,-17.30664c0,-9.941 8.059,-18 18,-18z'></path>
+							</g>
+						</g>
+					</svg>
+				</Link>
+			</div>
+			<p className='text-foreground-600'>Alif Pahreza 2025.</p>
+		</div>
+	);
+}
